441fd7a11095e2ddd6ea89b7a86d5ed8afdcd37d
phuctran2203 <101928319+phuctran2203@users.noreply.github.com>
2022-04-23 23:48:38 +0700

    Add files via upload

---
+++ app/graph_pages/table.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+import altair as alt
+import plotly.express as px
+from PIL import Image
+
+st.title("Table")
+
+# LOAD DATA
+
+data = pd.read_csv('data.csv', index_col=0)
+
+gb = GridOptionsBuilder.from_dataframe(data)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    data,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT',
+    update_mode='MODEL_CHANGED',
+    fit_columns_on_grid_load=False,
+    enable_enterprise_modules=True,
+    height=500,
+    width=1000,
+    reload_data=True
+)
+
+data = grid_response['data']
+selected = grid_response['selected_rows']
+df = pd.DataFrame(selected)

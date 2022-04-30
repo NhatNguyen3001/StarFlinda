048ddce328b1df8632856d300784bb387e183033
MiuX <s3911365@rmit.edu.vn>
2022-04-30 20:09:12 +1000

    css + default bar chart

--- app/graph_pages/bar_chart.py
+++ app/graph_pages/bar_chart.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 from turtle import title
 import streamlit as st
 import pandas as pd
@@ -22,13 +23,14 @@ def app():
 
     # Function
     # CSV file
-    csv_url = 'https://raw.githubusercontent.com/Lee-GaIn/-BIIT-Project/main/lib/data/data.csv'
-    df = pd.read_csv(csv_url)
+    csv = 'https://raw.githubusercontent.com/Lee-GaIn/-BIIT-Project/main/lib/data/data.csv'
+    df = pd.read_csv(csv)
 
     st.write("The bar chart show the Number of flight which each airline branches has in business and economy on a specific date from the user input. The statistic can only be view in latest 3 months.")
     st.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; **For example:** Today is 30th April 2022, the statistic can be view in any date from March to May 2022.")
     st.text("Please input following format: dd/mm/yyyy")
-    d = st.text_input('**Choose the date**')
+    # Declair text input
+    d = st.text_input('**Choose the date**', max_chars = 10)
 
 
     ###Price_bar
@@ -39,16 +41,26 @@ def app():
         column='airline:N'
     ).transform_filter(alt.FieldEqualPredicate(field='date', equal=d)
                     ).properties(height=500, width=60)
+    
+    pricechart_default = alt.Chart(df).mark_bar().encode(
+    y=alt.Y('count(airline):O', title="Number of flight"),
+    x=alt.X('class:N', title="Class"),
+    color='class:N',
+    column='airline:N'
+    ).properties(height=500, width=60)
+
 
     ##Price button
 
 
     if d:
         st.write(pricechart)
-
-        # Footer
-        st.markdown("""
-        <nav class="navbar fixed-bottom navbar-expand-xl navbar-dark" style="background-color: #234362; border-top-style: solid;">
-        <div style="padding-left: 650px; color: #FFFFFF; "> ©All Rights Reserved By STAFLINDA</div>
-        </nav>
-        """, unsafe_allow_html=True)
+    else:
+        st.write(pricechart_default)
+ 
+    # Footer
+    st.markdown("""
+    <nav class="navbar fixed-bottom navbar-expand-xl navbar-dark" style="background-color: #234362; border-top-style: solid;">
+    <div style="padding-left: 650px; color: #FFFFFF; "> ©All Rights Reserved By STAFLINDA</div>
+    </nav>
+    """, unsafe_allow_html=True)
